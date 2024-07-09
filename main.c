0e54642241ec52c3595b7f42c8928d6274cce7a6
aabdelghani <ahmedabdelghany15@gmail.com>
2024-07-09 23:08:22 +0300

    Adding more Tests

--- main.c
+++ main.c
@@ -1,29 +1,47 @@
 #include "custom_array.h"
 #include <stdio.h>
 
+void printArray(CustomArray *array) {
+    for (size_t i = 0; i < array->size; i++) {
+        printf("%d ", getElement(array, i));
+    }
+    printf("\n");
+}
+
 int main() {
     CustomArray array;
+    
+    // Initialization
     initArray(&array, 4);
-
+    printf("Array initialized with capacity 4.\n");
+    
+    // Insertion
     insertElement(&array, 10);
     insertElement(&array, 20);
     insertElement(&array, 30);
     insertElement(&array, 40);
-
-    printf("Initial elements:\n");
-    for (size_t i = 0; i < array.size; i++) {
-        printf("%d ", getElement(&array, i));
-    }
-    printf("\n");
-
+    printf("Elements inserted: ");
+    printArray(&array);
+    
+    // Resize
+    printf("Resizing array to capacity 10.\n");
+    resizeArray(&array, 10);
+    printf("New capacity: %zu\n", array.capacity);
+    
+    // Insertion after resize
+    insertElement(&array, 50);
+    printf("Element 50 inserted after resizing: ");
+    printArray(&array);
+    
+    // Access
+    printf("Access element at index 2: %d\n", getElement(&array, 2));
+    
+    // Deletion
     deleteElement(&array, 1);
-    printf("After deleting element at index 1:\n");
-    for (size_t i = 0; i < array.size; i++) {
-        printf("%d ", getElement(&array, i));
-    }
-    printf("\n");
-
-    freeArray(&array);
+    printf("Element at index 1 deleted: ");
+    printArray(&array);
+    
+    
     return 0;
 }
 
